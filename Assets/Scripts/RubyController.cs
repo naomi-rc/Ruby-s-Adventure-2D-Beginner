b4b5093e21f9588fe327d63967c99c3cd6e6b53c
naomi-rc <35537740+naomi-rc@users.noreply.github.com>
2021-12-21 22:04:40 -0500

    added base project; added player controls and tilemaps

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log(horizontal);
+        Vector2 position = transform.position;
+        position.x += 3.0f * horizontal * Time.deltaTime;
+        position.y += 3.0f * vertical * Time.deltaTime;
+        transform.position = position;
+
+    }
+}
